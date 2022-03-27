175a94cfa800c947c396a17fc0ca38b0e128d5aa
Tsukasa Hasegawa <41940715+thasegawa-305cc@users.noreply.github.com>
2022-03-27 14:48:43 +0900

    更新テスト

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 
 document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla SandBox !</h1>
+<h1>Hello Vanilla SandBox 2!</h1>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
